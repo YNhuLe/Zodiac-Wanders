7a53ed29ee5900ea3b4ce62d4b9f7a80ece649dc
NhuYLe <lenhuy10011996@gmail.com>
2025-02-27 15:16:59 -0500

    add details page

---
+++ front-end/src/pages/CardPage/CardPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardDetails from "../../components/CardDetail/CardDetail";
+
+function CardPage() {
+  return (
+    <>
+      <CardDetails />
+    </>
+  );
+}
+
+export default CardPage;
--- front-end/src/pages/Homepage/Homepage.jsx
+++ front-end/src/pages/Homepage/Homepage.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import ZodiacCard from "../../components/Cards/ZodiacCard";
 import Footer from "../../components/Footer/Footer";
+import CardDetails from "../../components/CardDetail/CardDetail";
 
 function Homepage() {
   return (
     <>
       <Header />
       <ZodiacCard />
-      <Header />
+      {/* <Header /> */}
+      <CardDetails />
       <Footer />
     </>
   );
