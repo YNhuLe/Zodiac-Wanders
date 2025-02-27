bb70c126d1f4d3257e69ed38dcfed6b1eaed8625
NhuYLe <lenhuy10011996@gmail.com>
2025-02-27 14:04:48 -0500

    add ZodiacCard

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
+import ZodiacCard from "./components/Cards/ZodiacCard";
 
 function App() {
-
   return (
     <>
-    
+      <ZodiacCard />
     </>
-  )
+  );
 }
 
 export default App
