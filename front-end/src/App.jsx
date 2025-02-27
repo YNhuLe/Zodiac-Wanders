754356cf48b63a82d7dd0019b17c2ee3268f87dc
Anzal-F <anzalmohamed09@gmail.com>
2025-02-27 13:36:08 -0500

    create routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
-
+  
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className="header__logo">
+            <p className="header-title">
+                Zodiac Wonders
+            </p>
+        </div>
+
+        <div className="header__info">
+            <h1>
+                Explore the Magic of Your Zodiac
+            </h1>
+        </div>
+    </div>
   )
 }
 
