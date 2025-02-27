16532a078c73b6c02691a94491a61effb1e43712
NhuYLe <lenhuy10011996@gmail.com>
2025-02-27 12:42:45 -0500

    modify index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import photosRoutes from "./routes/zodiac.js";
 import "dotenv/config";
 
 const port = process.env.PORT ?? 5050;
@@ -10,6 +11,8 @@ app.use(cors());
 //serve static files
 app.use(express.static("public"));
 app.use(express.json());
+
+app.use("/photos", photosRoutes);
 
 app.listen(port, () => {
   console.log(`Server is listening on https://localhost:${port}`);
--- routes/zodiac.js
+++ routes/zodiac.js
@@ -1,2 +1,16 @@
 import express from "express";
 import fs from "fs";
+ const router = express.Router();
+
+ router.get("/", (_req, res) => {
+   try {
+     const photo = fs.readFileSync("./data/zodiac.json");
+     const photAlbum = JSON.parse(photo);
+     res.json(photAlbum);
+   } catch (error) {
+     console.error("Error reading photo.json.", error);
+     res.status(500).json({ message: "Server error!" });
+   }
+ });
+
+ export default router;
